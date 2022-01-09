22cd2ac93b794c8a32a6f6780146d2945f4b237e
SamuelVasquezGonzalez <samuelgonza2006@gmail.com>
2022-01-09 07:51:55 -0500

    se agrego validaciones a cofla4

--- cofla4.js
+++ cofla4.js
@@ -17,68 +17,83 @@ promedioBoton.addEventListener("click", enviarNotas);
 
 function enviarNotas () {
 
-    notaAnteriorUno = 4;
-    notaAnteriorDos = 1.5;
-
-    anteriorFinal = notaAnteriorUno + notaAnteriorDos;
-
     const notaUno = document.querySelector(".notaUno").value;
     
+    let notaDos = 2;
+    let notaTres = 3;
+
     const obtenidaUno = document.querySelector(".obtenidaUno");
+    const obtenidaDos = document.querySelector(".obtenidaDos");
+    const obtenidaTres = document.querySelector(".obtenidaTres");
 
     obtenidaUno.innerHTML = notaUno;
+    obtenidaDos.innerHTML = notaDos;
+    obtenidaTres.innerHTML = notaTres;
 
     const promedioFinal = document.querySelector(".promedioFinal");
 
-    resultadoAhora = Math.round(notaAnteriorUno + notaAnteriorDos + notaUno  / 3);
+    resultadoFinal = Math.round(notaDos + notaTres + notaUno / 3);
+    promedioFinal.innerHTML = resultadoFinal;
     const mensaje = document.querySelector(".mensaje");
-    switch (resultadoAhora){
-        case 1 :
-            mensaje.innerHTML = "No pasaste la Materia";
-            mensaje.classList.add("perdiste")
-            break;
-        case 2:
-            mensaje.innerHTML = "No pasaste la Materia";
-            mensaje.classList.add("perdiste")
-            break;
-        case 3:
-            mensaje.innerHTML = "No pasaste la Materia";
-            mensaje.classList.add("perdiste")
-            break;
-        case 4:
-            mensaje.innerHTML = "No pasaste la Materia";
-            mensaje.classList.add("perdiste")
-            break;
-        case 5:
-            mensaje.innerHTML = "No pasaste la Materia";
-            mensaje.classList.add("perdiste")
-            break;
-        case 6:
-            mensaje.innerHTML = "No pasaste la Materia";
-            mensaje.classList.add("perdiste")
-            break;
-        case 7:
-            mensaje.innerHTML = "Pasaste la Materia Raspado";
-            mensaje.classList.add("ganaste")
-            break;
-        case 8:
-            mensaje.innerHTML = "Pasaste pero pudiste estar mejor";
-            mensaje.classList.add("ganaste")
-            break;
-        case 9:
-            mensaje.innerHTML = "Excelente!!";
-            mensaje.classList.add("ganaste")
-            break;
-        case 10:
-            mensaje.innerHTML = "Perfecto!!";
-            mensaje.classList.add("ganaste")
-            break;
-        default:
-            mensaje.innerHTML = "que paso?"
-    }
 
-    const info = document.querySelector(".tarjeta-wrapper")
-    promedioFinal.innerHTML = resultadoAhora;
+    if(notaUno > 10 ){
+        obtenidaUno.innerHTML = "";
+        obtenidaDos.innerHTML = "";
+        obtenidaTres.innerHTML = "";
+        resultadoAhora = "";
+        mensaje.innerHTML = "No puedes tener mas de 10 de nota";
+        mensaje.classList.add("perdiste");
+        promedioFinal.innerHTML = "";
+        
+    }else{
+        switch (resultadoFinal){
+            case 1 :
+                mensaje.innerHTML = "No pasaste la Materia";
+                mensaje.classList.add("perdiste")
+                break;
+            case 2:
+                mensaje.innerHTML = "No pasaste la Materia";
+                mensaje.classList.add("perdiste")
+                break;
+            case 3:
+                mensaje.innerHTML = "No pasaste la Materia";
+                mensaje.classList.add("perdiste")
+                break;
+            case 4:
+                mensaje.innerHTML = "No pasaste la Materia";
+                mensaje.classList.add("perdiste")
+                break;
+            case 5:
+                mensaje.innerHTML = "No pasaste la Materia";
+                mensaje.classList.add("perdiste")
+                break;
+            case 6:
+                mensaje.innerHTML = "No pasaste la Materia";
+                mensaje.classList.add("perdiste")
+                break;
+            case 7:
+                mensaje.innerHTML = "Pasaste la Materia Raspado";
+                mensaje.classList.add("ganaste")
+                break;
+            case 8:
+                mensaje.innerHTML = "Pasaste pero pudiste estar mejor";
+                mensaje.classList.add("ganaste")
+                break;
+            case 9:
+                mensaje.innerHTML = "Excelente!!";
+                mensaje.classList.add("ganaste")
+                break;
+            case 10:
+                mensaje.innerHTML = "Perfecto!!";
+                mensaje.classList.add("ganaste")
+                break;
+            default:
+                mensaje.innerHTML = "que paso?"
+        }
+    }
+    
+
+    const info = document.querySelector(".tarjeta-wrapper");
     info.removeChild(promedioBoton);
 
 }
